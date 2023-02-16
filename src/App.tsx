0862d1cf39d90fa65291ab55acc38ecad1c19c8c
Adam Wozniak <adamwozhere@gmail.com>
2023-02-16 23:01:46 +0000

    frontendmentor setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Form from './components/Form';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="wrapper">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
